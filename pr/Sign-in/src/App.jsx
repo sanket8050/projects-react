c7a9130e69887c7222a394a8c0787982740b956b
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-08-10 23:58:18 +0530

    q

--- pr/Sign-in/src/App.jsx
+++ pr/Sign-in/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 import Form from '../component/Form'
 import './App.css'
+import Select from '../component/Select'
+import Navbar from '../component/Navbar'
+
+
+import {  Routes,Route  } from 'react-router-dom' 
+import Home from '../component/Home'
+import Blogs from '../component/Blogs'
+import Contact from '../component/Contact'
+
 function App() {
  
 
   return (
    <>
-  <Form />
-  
-   
+  {/* <Form />
+  <Select/>
+  <Navbar/> */}
+  <Navbar/>
+
+  <Routes>
+    <Route path='/' element={<Home/> }/>
+    <Route path='/Blogs' element={<Blogs/> }/>
+    <Route path='/Contact' element={<Contact/> }/>
+  </Routes>
+        
    </>
   )
 }
