9202f1792f204982acc5accd12c2ad4df1cdcc5d
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-08-11 23:51:04 +0530

    d

---
+++ pr/Pages/pages/Navbar.jsx
@@ -0,0 +1,31 @@
+
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+   <div className=' '>
+     <nav className=''>
+        <ul className="flex gap-6">
+            <li>
+                <Link to="/Home">Home</Link>
+            </li>
+            <li>
+                <Link to="/About">About</Link>
+            </li>
+            <li>
+                <Link to="/Blog">Blog</Link>
+            </li>
+
+            <li>
+                <Link to="*"></Link>
+            </li>
+        </ul>
+
+    </nav>
+   </div>
+    </>
+  )
+}
+
+export default Navbar
