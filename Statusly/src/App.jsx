125ce4784c48eb0e2d6100ab1f23c6b88d90fe1a
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-08-12 17:40:31 +0530

    S

---
+++ Statusly/src/App.jsx
@@ -0,0 +1,42 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
+import Home from './pages/Home.jsx'
+import ShopPage from './pages/ShopPage.jsx'
+import OwnerDashboard from './pages/OwnerDashboard.jsx'
+import Login from './pages/Login.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop/:id" element={<ShopPage />} />
+          <Route path="/owner" element={<OwnerDashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
+
+export default App
---
+++ Statusly/src/components/common/Button.jsx
@@ -0,0 +1,5 @@
+import { Button as ShadcnButton } from '@shadcn/ui'
+
+export default function Button(props) {
+  return <ShadcnButton {...props} />
+}
