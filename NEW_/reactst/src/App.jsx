7838cd967286c945578137e40ff2497bdb5268e3
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-08-21 23:37:08 +0530

    app

---
+++ NEW_/reactst/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
+        </button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>{data.length > 0 
+            ? (
+              data   .map((user, index) => (
+                <div key={index}>
+                  <p>{user.xname}</p>
+                  <p>{user.username}</p>
+                </div>
+              ))
+            ) : (
+              <p>No data available</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
