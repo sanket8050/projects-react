1e53eeb66ecd164968560dacd25e54db58fbfdee
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-01 23:57:30 +0530

    update

--- razorpay-mern p2/frontend/src/components/Card.jsx
+++ razorpay-mern p2/frontend/src/components/Card.jsx
@@ -1,12 +1,20 @@
-import { VStack } from '@chakra-ui/react'
-import React from 'react'
+import React from 'react';
 
-function Card({amount,img}) {
+function Card({ img, amount, checkout }) {
   return (
-    <VStack>
-        <image src=
-    </VStack>
-  )
+    <div className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center w-48 ">
+      <img className="h-32 w-32 object-cover rounded-md" src={img} alt="product" />
+      <p className="mt-2 text-lg font-semibold text-gray-800 text-center">
+        ₹{amount}
+      </p>
+      <button
+        onClick={()=>{checkout(amount)}}
+        className="mt-3 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+      >
+        Buy now
+      </button>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
