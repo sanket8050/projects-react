599a4cad1f9ece840ecdb15ffe3b50b53de51f5a
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-01 23:57:20 +0530

    update

--- razorpay-mern p2/frontend/src/App.jsx
+++ razorpay-mern p2/frontend/src/App.jsx
@@ -1,20 +1,31 @@
 
-import  {BrowserRouter as Router, Routes,Route} from "react-router-dom"
 import './App.css'
-import Home from "./components/Home"
+import Card from "../src/components/Card"
+import axios from 'axios'
+
+
 
 function App() {
-
+  const checkout = async (amount)=>{
+    const { data } =  await axios.post("http://localhost:4000/api/checkout",{
+      amount
+    })
+      console.log(data);
+    
+  }
+  
+  
+ 
 
   return (
-  <>
-  <Router>
-    <Routes>
-      <Route  path="/" element={<Home/>}/>
-    </Routes>
-  </Router>
+  <div className="flex flex-col md:flex-row gap-4 items-center  ">
+    <Card amount={5000} img="https://cdn.pixabay.com/objects3d/2025/08/11/09-16-28-881/render_720_720_0_340_0.png" checkout={checkout}/>
 
-  </>
+    <Card amount={3000} img="https://cdn.pixabay.com/photo/2017/04/03/15/52/mobile-phone-2198770_1280.png" checkout={checkout}/>
+
+    <Card  img={"https://cdn.pixabay.com/photo/2025/08/26/16/58/guy-9798371_1280.png"} amount={500} checkout={checkout}  />
+</div>
+
   )
 }
 
