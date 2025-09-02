bd443667e11289ba9f0aa2e8022eca6f253df605
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-02 23:38:59 +0530

    MERN STACK

--- razorpay-mern p2/server/app.js
+++ razorpay-mern p2/server/app.js
@@ -7,4 +7,8 @@ config({ path : "./config/config.env"});
 
 export const app = express();
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use("/api",paymentroute)
+
+app.get("/api/getkey",(req,res)=>res.status(200).json({key:process.env.RAZORPAY_API_KEY}))
