d48ac9ce8a2df8c876253b382efccf6cc09a3ac1
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-05 22:31:21 +0530

    changed

---
+++ OMR/server/omr_processor.py
@@ -0,0 +1,9 @@
+def process_omr(image_path):
+    # TODO: Add OpenCV logic here
+    # For now, return dummy answers
+    return {
+        "Q1": "B",
+        "Q2": "C",
+        "Q3": "A",
+        "Q4": "D"
+    }
