8e9f62ac9577a5d521fc8bf4881493c827cba13f
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-15 21:56:14 +0530

    rendering

---
+++ live_data_rendering/next/app/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Form from "../components/Form";
+// import Form from "../components/form";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center ">
+    <Form/>
+   
+    </main>
+  );
+}
