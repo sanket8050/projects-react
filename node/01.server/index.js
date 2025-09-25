4662ff00a5bdcfd78f29cd0acaf55ff5be97c706
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-25 23:19:50 +0530

    updating

--- node/01.server/index.js
+++ node/01.server/index.js
@@ -46,29 +46,58 @@
 
 
 //**************************************************** */
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
+// app.use(express.json());
+
+// // Middleware - log API call with timestamp
+// app.use((req, res, next) => {
+//   console.log(`API called at: ${new Date().toLocaleTimeString()} | ${req.method} ${req.url}`);
+//   next();
+// });
+
+// // Query Param Example
+// app.get("/products", (req, res) => {
+//   const { category, price } = req.query;
+//   res.send(`Showing products in category ${category} under price ${price}`);
+// });
+
+// // Route Param Example (bonus)
+// app.get("/hello/:category/:price", (req, res) => {
+//   const { category, price } = req.params;
+//   res.send(`Price and category: ${category}, ${price}`);
+// }); 
+
+// app.listen(3000, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
+
+
+
+const express = require('express')
+const app = express()
+const port = 3000
 app.use(express.json());
 
-// Middleware - log API call with timestamp
-app.use((req, res, next) => {
-  console.log(`API called at: ${new Date().toLocaleTimeString()} | ${req.method} ${req.url}`);
-  next();
-});
+function addmidleware(req,res,next){
+    console.log(req.method);
+    console.log(req.hostname);
+    console.log(req.url);
+    
+    
+    next();
+}
+app.use(addmidleware);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// Query Param Example
-app.get("/products", (req, res) => {
-  const { category, price } = req.query;
-  res.send(`Showing products in category ${category} under price ${price}`);
-});
+app.get('/product', (req, res) => {
+  res.send('xxx World!')
+})
 
-// Route Param Example (bonus)
-app.get("/hello/:category/:price", (req, res) => {
-  const { category, price } = req.params;
-  res.send(`Price and category: ${category}, ${price}`);
-}); 
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
